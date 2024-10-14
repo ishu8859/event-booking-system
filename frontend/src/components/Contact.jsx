51c825a06ddf8718742e6c2300850d7254fb628f
Ishu <kashyapishwar52@gmail.com>
2024-10-15 05:09:13 +0530

    first commit

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,105 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        {
+          name,
+          email,
+          subject,
+          message,
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSubject("");
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  return (
+    <>
+      <div className="contact container">
+        <div className="banner">
+          <div className="item">
+            <h4>Address</h4>
+            <p>Any where, Any City</p>
+          </div>
+          <div className="item">
+            <h4>Call Us</h4>
+            <p>Call Us: +91-88591-96712</p>
+          </div>
+          <div className="item">
+            <h4>Mail Us</h4>
+            <p>kashyapishwar52@gmail.com</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="item">
+            <iframe
+              src="https://www.google.com/maps/search/india+gate/@28.6090561,77.2226052,15z/data=!3m1!4b1?hl=en&entry=ttu&g_ep=EgoyMDI0MTAwOS4wIKXMDSoASAFQAw%3D%3D"
+              style={{ border: 0, width: "100%", height: "450px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
+          <div className="item">
+            <form onSubmit={handleSendMessage}>
+              <h2>CONTACT</h2>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  type="email"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="Subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+              <textarea
+                rows={10}
+                placeholder="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="banner">
+        <div className="title">
+          <h1>Event</h1>
+          <p>Events and Weddings</p>
+        </div>
+        <div className="tag">
+          <label>News Letter</label>
+          <div>
+            <input type="text" placeholder="E-mail" />
+            <button>Subscribe</button>
+          </div>
+          <p>Sign up with your email address to receive news and updates!</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
